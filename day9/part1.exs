c50668e78d5ae72c78998a238af81e3ba3ec8b49
Lorenzo Cocciarelli <l.cocciarelli@arkemis.it>
2025-12-09 09:22:58 +0100

    day9p1

---
+++ day9/part1.exs
@@ -0,0 +1,17 @@
+all_pairs = fn points ->
+  for i <- 0..(length(points) - 2),
+      j <- (i + 1)..(length(points) - 1) do
+    {Enum.at(points, i), Enum.at(points, j), i, j}
+  end
+end
+
+File.stream!("input")
+|> Stream.map(&String.trim/1)
+|> Enum.map(&String.split(&1, ","))
+|> Enum.map(&Enum.map(&1, fn n -> String.to_integer(n) end))
+|> all_pairs.()
+|> Enum.map(fn {[x1, y1], [x2, y2], i, j} ->
+  {(abs(x2 - x1) + 1) * (abs(y2 - y1) + 1), i, j}
+end)
+|> Enum.max(fn {area_1, _i, _j}, {area_2, _i2, _j2} -> area_1 > area_2 end)
+|> IO.inspect()
