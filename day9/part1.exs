12242f96d0282aff84483728023de8172a64c0ba
Lorenzo Cocciarelli <l.cocciarelli@arkemis.it>
2025-12-09 09:29:02 +0100

    whynot

--- day9/part1.exs
+++ day9/part1.exs
@@ -1,15 +1,10 @@
-all_pairs = fn points ->
-  for i <- 0..(length(points) - 2),
-      j <- (i + 1)..(length(points) - 1) do
-    {Enum.at(points, i), Enum.at(points, j), i, j}
-  end
-end
-
 File.stream!("input")
 |> Stream.map(&String.trim/1)
 |> Enum.map(&String.split(&1, ","))
 |> Enum.map(&Enum.map(&1, fn n -> String.to_integer(n) end))
-|> all_pairs.()
+|> then(
+  &for {pi, i} <- Enum.with_index(&1), {pj, j} <- Enum.with_index(&1), i < j, do: {pi, pj, i, j}
+)
 |> Enum.map(fn {[x1, y1], [x2, y2], i, j} ->
   {(abs(x2 - x1) + 1) * (abs(y2 - y1) + 1), i, j}
 end)
